858e081468f01ae6799fea4c48c28dd6180e4cda
Daniel Stutzman <dtstutz@gmail.com>
2013-10-03 17:05:14 -0600

    Put black border around list bullets with X123 in front.

--- app/models/lesson_plan.rb
+++ app/models/lesson_plan.rb
@@ -8,6 +8,7 @@ class LessonPlan < ActiveRecord::Base
 
   def content_as_html
     last_num_indents = 0
+
     lines = self.content.split(/\r?\n/).map do |line|
       if match = line.match(/^\* (.*)$/)
         num_indents = 0
@@ -16,32 +17,45 @@ class LessonPlan < ActiveRecord::Base
         num_indents = 0
         line = "<h2>#{match[1]}</h2>" # convert ** to h2
       else
-        match = line.match(/^( *)(- )?(.*)$/)
+        match = line.match(/^( *)(- )?(X([0-9]+) ?)?(.*)$/)
         num_indents = match[1].size + (match[2] || '').size
 
-        line = match[3]
+        line = match[5]
+
+        possible_class = ''
+        possible_intro = ''
+        if exercise_dir = match[4]
+          possible_class = "class='exercise'"
+          possible_intro = "<b>Exercise #{exercise_dir}</b> "
+        end
 
         if match[2] == '- '
-          line = "<li>#{line}"
+          line = "<li #{possible_class}>#{possible_intro}" + line
         else
-          line = "<br>#{line}"
+          line = '<br>' + line
         end
       end
 
-      if num_indents > last_num_indents
-        last_num_indents += 2
+      while num_indents > last_num_indents
         line = '<ul>' + line
+        last_num_indents += 2
       end
 
       while num_indents < last_num_indents
-        last_num_indents -= 2
         line = '</ul>' + line
+        last_num_indents -= 2
       end
 
       last_num_indents = num_indents
 
       line
     end
+
+    while last_num_indents > 0
+      last_num_indents -= 2
+      lines += ['</ul>']
+    end
+
     lines.join("\n").html_safe
   end
 end
