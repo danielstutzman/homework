312c03f2f4184c8c1e634d5de9be9e5fbe4837e8
Daniel Stutzman <dtstutz@gmail.com>
2013-10-06 16:54:45 -0600

    Make exercise formatting more readable.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -6,4 +6,8 @@ class Exercise < ActiveRecord::Base
   validates :first_line, presence: true
   validates :content,    presence: true
   validates :order_in_lesson, presence: true
+
+  def content_as_html
+    self.content.gsub(/`(.*?)`/, '<code>\\1</code>').gsub(/(https?:\/\/[^\n ,]+)/, "<code><a target='_new' href='\\1'>\\1</a></code>").html_safe
+  end
 end
