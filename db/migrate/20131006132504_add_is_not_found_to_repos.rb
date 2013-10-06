7e33c8412b38b9d03a43e47c6af342bfac4324fe
Daniel Stutzman <dtstutz@gmail.com>
2013-10-06 07:53:02 -0600

    Oops, need a default value for repos.is_not_found

--- db/migrate/20131006132504_add_is_not_found_to_repos.rb
+++ db/migrate/20131006132504_add_is_not_found_to_repos.rb
@@ -1,5 +1,5 @@
 class AddIsNotFoundToRepos < ActiveRecord::Migration
   def change
-    add_column :repos, :is_not_found, :boolean, null: false
+    add_column :repos, :is_not_found, :boolean, null: false, default: false
   end
 end
