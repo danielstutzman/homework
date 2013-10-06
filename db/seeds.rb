7b4ef465a5a79cbc0c7051ba99cfa62e169e71a7
Daniel Stutzman <dtstutz@gmail.com>
2013-10-06 07:22:57 -0600

    Load seeds in a transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,36 @@
-daniel = User.create! github_username: 'danielstutzman'
+User.transaction do
 
-homework_repo = Repo.create! \
-  user:      daniel,
-  name:      'homework',
-  https_url: 'https://github.com/danielstutzman/homework',
-  has_exercise_dirs: false,
-  is_not_found: false # will be set to true upon login
+  daniel = User.create! github_username: 'danielstutzman'
 
-lesson1 = LessonPlan.create! \
-  date: Date.new(2013, 10, 7),
-  topic: 'First Day',
-  content: "This is a test"
+  homework_repo = Repo.create! \
+    user:      daniel,
+    name:      'homework',
+    https_url: 'https://github.com/danielstutzman/homework',
+    has_exercise_dirs: false,
+    is_not_found: false # will be set to true upon login
 
-one_hundred = Exercise.create! \
-  id: 100,
-  lesson_plan: lesson1,
-  first_line: 'test'
+  lesson1 = LessonPlan.create! \
+    date: Date.new(2013, 10, 7),
+    topic: 'First Day',
+    content: "This is a test"
 
-Refresh.create! \
-  user: daniel,
-  repo: homework_repo,
-  exercise_dir: '100',
-  exercise: one_hundred,
-  logged_at: DateTime.now
+  one_hundred = Exercise.create! \
+    id: 100,
+    lesson_plan: lesson1,
+    first_line: 'test'
 
-SidebarLink.create! \
-  name: 'Gallery',
-  url: 'http://dvc-projects.tumblr.com'
-SidebarLink.create! \
-  name: 'HipChat',
-  url: 'https://davincicoders.hipchat.com/home'
+  Refresh.create! \
+    user: daniel,
+    repo: homework_repo,
+    exercise_dir: '100',
+    exercise: one_hundred,
+    logged_at: DateTime.now
+
+  SidebarLink.create! \
+    name: 'Gallery',
+    url: 'http://dvc-projects.tumblr.com'
+  SidebarLink.create! \
+    name: 'HipChat',
+    url: 'https://davincicoders.hipchat.com/home'
+
+end # end transaction
